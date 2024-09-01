143403e00cfcfb39f79583b846b8044ff90453c9
Anton Chahan <chaganton@gmail.com>
2024-09-02 00:13:39 +0200

    1. Add Spanish. 2. Add bilds for deploy.

---
+++ dist/portfolio/server/en-US/main.server.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-PTNFRKLU.mjs";import"./chunk-V3AK3T2Q.mjs";import"./chunk-4AJ3OW2J.mjs";import"./chunk-OD3KUHZZ.mjs";export{a as default};
+/**i18n:ad3c6398ec8af3938f5c3ce63e13f7dbd7c181d88fc202cf5b7a10c6e5d32193*/
