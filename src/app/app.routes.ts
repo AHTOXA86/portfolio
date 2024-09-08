38db1ab470f759b7e7ebfaee8ff3ca4187c1dc7d
Anton Chahan <chaganton@gmail.com>
2024-09-08 19:12:55 +0200

    Add left pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component'
 import { ProjectsComponent } from "./projects/projects.component";
+import { LanguagesComponent } from './languages/languages.component';
+import { EducationComponent } from './education/education.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +14,15 @@ export const routes: Routes = [
         path: 'projects',
         title: 'Projects',
         component: ProjectsComponent,
+      },
+    {
+        path: 'languages',
+        title: 'Languages',
+        component: LanguagesComponent,
+      },
+    {
+        path: 'education',
+        title: 'Education',
+        component: EducationComponent,
       },
 ];
--- src/app/technical-skills/technical-skills.component.ts
+++ src/app/technical-skills/technical-skills.component.ts
@@ -8,9 +8,24 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { category: 'PROGRAMMING LANGUAGES', values: 'Python, TypeScript, PHP, HAML ' }
+  { category: 'PROGRAMMING LANGUAGES AND TECHNOLOGIES', values: 'Python, TypeScript, HAML, HTML5, CSS3, SASS, JavaScript, UML, PHP, C/C++' },
+  { category: 'WEB SERVERS', values: 'Gunicorn, Sshd, Apache HTTPD, Nginx, FTP ' },
+  { category: 'VERSION CONTROL TOOLS', values: 'Git, GitLab, Bitbucket ' },
+  { category: 'OPERATING SYSTEMS ', values: 'macOS, Linux, Windows, Android (desktop)' },
+
 ];
 
+const ELEMENT_DATA2: PeriodicElement[] = [
+
+  { category: 'FRAMEWORKS AND LIBRARIES ', values: 'Django, Flask, aiohttp, Silex, React.js, Angular ' },
+  { category: 'WEB TECHNOLOGIES ', values: 'JavaScript ES6, JSON, XML, CSS, HTML/DHTML, RESTful API ' },
+  { category: 'SHELL SCRIPTING ', values: 'GBash, Zsh ' },
+  { category: 'DEVELOPMENT TOOLS ', values: 'Docker, Kubernetes, Pytest, Jira, Jenkins, VirtualBox, Vagrant, PyLint, ESLint, pre-commit' },
+  { category: 'SERVICES', values: 'AWS' },
+  { category: 'DATABASES', values: 'PostgreSQL, MySQL, MongoDB ' },
+  { category: 'METHODOLOGIES', values: 'TDD, Continuous Integration, Scrum ' },
+ 
+];
 
 @Component({
   selector: 'app-technical-skills',
@@ -22,4 +37,5 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class TechnicalSkillsComponent {
   displayedColumns: string[] = ['category', 'values'];
   dataSource = ELEMENT_DATA;
+  dataSource2 = ELEMENT_DATA2;
 }
