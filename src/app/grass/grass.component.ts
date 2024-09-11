0229495b7b79040fc594b813e9dabd1515df0de5
Anton Chahan <chaganton@gmail.com>
2024-09-11 17:53:06 +0200

    add grass component. update build.

---
+++ src/app/grass/grass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grass',
+  standalone: true,
+  imports: [],
+  templateUrl: './grass.component.html',
+  styleUrl: './grass.component.scss'
+})
+export class GrassComponent {
+
+}
