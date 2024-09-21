2ed88d7fa8ec4835f3639a9a8bf427a1ea16feaa
Anton Chahan <chaganton@gmail.com>
2024-09-22 00:49:55 +0200

    Add all data into json file.

---
+++ src/app/intefaces.ts
@@ -0,0 +1,11 @@
+interface Project {
+  company: string;
+  customer: string;
+  description: string;
+  teamSize: number;
+  involvementDuration: string;
+  role: string;
+  responsibilities: string[];
+  toolsAndTechnologies: string[];
+  project: string;
+}
