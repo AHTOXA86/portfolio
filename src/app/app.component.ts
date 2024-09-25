5a648c66a662f6d1b171772da593733c56ecbc13
Anton Chahan <chaganton@gmail.com>
2024-09-25 20:46:35 +0200

    Finish layout of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavmenuComponent } from './navmenu/navmenu.component'
 import { NgOptimizedImage } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavmenuComponent, NgOptimizedImage],
+  imports: [RouterOutlet, NavmenuComponent, NgOptimizedImage, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
