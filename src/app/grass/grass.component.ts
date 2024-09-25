aff2687d00212a1a65d2645a9da92fa10a966889
Anton Chahan <chaganton@gmail.com>
2024-09-25 23:24:17 +0200

    Add cube for test 3d scene

--- src/app/grass/grass.component.ts
+++ src/app/grass/grass.component.ts
@@ -1,14 +1,25 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { extend } from 'angular-three';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { extend, NgtBeforeRenderEvent, NgtArgs, NGT_STORE} from 'angular-three';
+import { OrbitControls } from 'three-stdlib';
 import * as THREE from 'three';
 
 extend(THREE);
+extend({ OrbitControls });
 
 @Component({
     standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [NgtArgs],
   templateUrl: './grass.component.html'
 })
 export class GrassComponent {
+  onBeforeRender(event: NgtBeforeRenderEvent<THREE.Mesh>) {
+    event.object.rotation.x += 0.01;
+}
+
+
+private readonly store = inject(NGT_STORE);
+readonly camera = this.store.get('camera');
+readonly glDom = this.store.get('gl', 'domElement');
 
 }
